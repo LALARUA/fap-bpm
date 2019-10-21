7560602dda758cc6cbc5286d92855e266959450e
zhongxiang <745402208@qq.com>
2019-10-21 17:48:15 +0800

    初始化项目

---
+++ src/main/java/com/venus/fapbpm/FapBpmApplication.java
@@ -0,0 +1,13 @@
+package com.venus.fapbpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FapBpmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FapBpmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/venus/fapbpm/FapBpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venus.fapbpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FapBpmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
